1922a08e0637ef96b7874c8978fd1fa1f30aa8fb
LatDatCat <vinh.np2419019@sis.hust.edu.vn>
2024-10-23 02:13:06 +0700

    add baitap 1

---
+++ Congtrunhan.cpp
@@ -0,0 +1,21 @@
+//Tính tổng hai số
+
+#include <iostream>
+using namespace std;
+int main() {
+    //Khai báo biến
+    float a, b;
+
+    //Nhap hai so
+     cout<<"Nhap a: ";
+     cin>> a;
+     cout<<"Nhap b: ";
+     cin>> b;
+
+    float S = a+b;
+
+    // Tổng hai số là
+     cout<<" Tong hai so can tim la " << S <<endl;
+
+     return 0;
+}
---
+++ Congtrunhan.py
@@ -0,0 +1,10 @@
+# Tính tổng hai số
+# Nhập dữ liệu hai số
+a = float(input("Nhap a: "))
+b = float(input("Nhap b: "))
+
+# Tính tổng hai số
+S = a + b
+
+# In kết quả
+print("Ket qua tong hai so la S =", S)
